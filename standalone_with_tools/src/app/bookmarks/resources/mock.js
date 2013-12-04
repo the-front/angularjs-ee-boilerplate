092e0347b11f44fffb1d0e33879d53e839332568
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-12-03 23:30:18 -0200

    app bookmarks mock

---
+++ standalone_with_tools/src/app/bookmarks/resources/mock.js
@@ -0,0 +1,14 @@
+angular.mock.backend.addResource(function($httpBackend, regexpUrl, getParams) {
+  'use strict';
+
+  console.log('TODO: define app/bookmarks mock');
+
+  //--- URL intercept
+
+  // TODO: define
+
+  //--- fake database
+
+  // TODO: define
+
+});
