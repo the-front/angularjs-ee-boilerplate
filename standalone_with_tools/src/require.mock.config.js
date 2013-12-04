fdb64bc98ec5ecd0940fe2c86ecf4b2cef9c8cb1
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2013-12-03 23:29:41 -0200

    create require mock config

---
+++ standalone_with_tools/src/require.mock.config.js
@@ -0,0 +1,64 @@
+require({
+
+  // libraries dependencies with fallback
+  paths: {
+
+    'angular-mocks': [
+      'vendor/angular.js/1.2.1/angular-mocks'
+    ],
+
+    'angular-mocks-backend': [
+      'vendor/angular-mocks-backend/0.1.3/angular-mocks-backend'
+    ]
+
+  },
+
+  // define js scripts dependencies
+  shim: {
+
+    'angular-mocks': {
+      deps: [
+        'angular_route', 
+        'angular_resource'
+      ]
+    },
+
+    'angular-mocks-backend': {
+      deps: ['angular-mocks']
+    },
+
+    'app/main/mock/allow-pass-jsonp': {
+      deps: ['angular-mocks-backend']
+    },
+
+    'app/help/mock/allow-pass-github': {
+      deps: ['angular-mocks-backend']
+    },
+
+    'app/bookmarks/resources/mock': {
+      deps: ['angular-mocks-backend']
+    }
+
+  }
+
+},
+
+['require'], function(require) {
+
+  console.log('project require mock config');
+
+  console.log(GLOBAL.appModuleDeps);
+
+  // update app module dependencies array
+  GLOBAL.appModuleDeps = GLOBAL.appModuleDeps.concat(['ngMockBackend']);
+
+  console.log(GLOBAL.appModuleDeps);
+
+  // start
+  require([
+    'app/main/mock/allow-pass-jsonp',
+    'app/help/mock/allow-pass-github',
+    'app/bookmarks/resources/mock'
+  ]);
+
+});
