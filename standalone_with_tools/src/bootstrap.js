03deea75be8a342f263da9eb5a10d947ae5cf22c
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-03-10 20:44:33 -0300

    angular-ee swt : new start app defined

---
+++ standalone_with_tools/src/bootstrap.js
@@ -0,0 +1,31 @@
+define(
+// require.js dependency injection
+[
+  'angular',
+
+  './shared/mock/require.load',
+  './app/main/require.load'
+], 
+
+// require.js module scope
+function(ng) {
+  'use strict';
+
+  console.log('bootstrap application');
+
+  // define run module to bootstrap application
+  ng.module(
+    // module name
+    'run',
+
+    // module dependencies
+    [
+      'ngMockBackend',
+      'main'      
+    ]
+  );
+
+  // start angular app
+  ng.bootstrap(document, ['run']);
+
+});
