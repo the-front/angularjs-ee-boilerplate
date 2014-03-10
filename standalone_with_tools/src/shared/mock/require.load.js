e63e3d5ed9712bd116d0927d299b29b1d900e045
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-03-10 20:45:55 -0300

    angular-ee swt : mock require load js files from /shared/mock

---
+++ standalone_with_tools/src/shared/mock/require.load.js
@@ -0,0 +1,14 @@
+define(
+// require.js dependency injection
+[
+  './require.config',
+
+  './module',
+  './backend',
+  './helpers',
+  './in-memory.db',
+  './allow-pass-jsonp'
+], 
+
+// require.js module scope
+function() {});
