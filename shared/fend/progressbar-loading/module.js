dbaf97665f4105d9d2fd091f5e1b6d3e58ab6115
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-03-11 22:16:10 -0300

    gh-pages auto commit 2014-03-12T01:16:10Z

--- shared/fend/progressbar-loading/module.js
+++ shared/fend/progressbar-loading/module.js
@@ -1,13 +1,16 @@
 define(
 // require.js dependency injection
 [
+  'require',
   'angular',
   'ngProgress'
 ], 
 
 // require.js module scope
-function(ng) {
+function(require, ng) {
   'use strict';
+
+  console.log(require.toUrl('ngProgress'));
 
   // module definition
   return ng.module(
