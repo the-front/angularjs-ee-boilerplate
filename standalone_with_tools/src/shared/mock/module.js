bd4e7a31047294a0b093b20ff216b2e832a577c8
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-03-17 15:08:54 -0300

    mock module code review

--- standalone_with_tools/src/shared/mock/module.js
+++ standalone_with_tools/src/shared/mock/module.js
@@ -1,34 +1,3 @@
-require.config({
-
-  // libraries dependencies with fallback
-  paths: {
-
-    angularMocks: [
-      'vendor/angular.js/1.2.1/angular-mocks'
-    ],
-
-    angularMocksBackend: [
-      'vendor/angular-mocks-backend/0.1.3/angular-mocks-backend'
-    ]
-
-  },
-
-  // define js scripts dependencies
-  shim: {
-
-    'angularMocks': {
-      deps: ['angular']
-    },
-
-    'angularMocksBackend': {
-      deps: ['angularMocks']
-    }
-
-  }
-
-});
-
-
 define(
 // require.js dependency injection
 [
