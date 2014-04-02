7b513b9a0f2c957e828e360ed5eeffda1cbd736e
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-04-01 21:40:52 -0300

    old files removed

--- helpers/grunt/config/gitclone.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  gh_pages: {
-    options: {
-      repository: '<%= package.repository.url %>',
-      branch: 'gh-pages',
-      directory: '<%= project.paths.gh_pages %>'
-    }
-  }
-};
