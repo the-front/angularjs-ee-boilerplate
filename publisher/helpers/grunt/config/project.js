60fc0faa5fd0fa60e80ef6b122c96780b62ef9f4
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-04-01 22:27:41 -0300

    update project.js config for grunt tasks

--- publisher/helpers/grunt/config/project.js
+++ publisher/helpers/grunt/config/project.js
@@ -5,9 +5,9 @@ module.exports = {
   paths: {
     gh_pages: '.local/gh_pages',
     project: {
-      dev: '../standalone_with_tools/src',
-      prod: '../standalone_with_tools/dist',
-      tools: '../standalone_with_tools/tools'
+      dev: '../src',
+      prod: '../dist',
+      tools: '../tools'
     }
   }
 
