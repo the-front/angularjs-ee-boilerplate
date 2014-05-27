748ff50617761d7e4013f31acbaaf4ce88cb70f9
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-05-27 16:35:10 -0300

    dev code : gh-pages auto commit 2014-05-27T19:35:10Z (UTC)

---
+++ shared/mock/in-memory.db.js
@@ -0,0 +1,30 @@
+// http://jshint.com/docs/options/
+/* jshint newcap:false */
+define(
+// require.js dependency injection
+[
+  'shared/mock/module',
+  'lokijs'
+],
+
+// require.js module scope
+function(module) {
+  'use strict';
+
+  module.factory(
+
+    // factory name
+    'DataStore',
+
+    // factory dependencies injection
+    [
+
+  // factory definition
+  function() {
+
+    // http://lokijs.org/
+    return new loki('mock.db');
+
+  }]);
+
+});
