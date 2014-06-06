d8e9a2ce0ec404fcfdca5491c7efc680a05e3755
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-06-06 17:04:11 -0300

    dev code : gh-pages automated commit 2014-06-06T20:04:11Z (UTC)

---
+++ app/bookmarks/resources/search.js
@@ -0,0 +1,32 @@
+define(
+// require.js dependency injection
+[
+  '../module'
+],
+
+// require.js module scope
+function(module) {
+  'use strict';
+
+
+  module.factory(
+
+    // resource name
+    'BookmarksSearchResource',
+
+    // dependency injection
+    ['$resource',
+
+  function($resource) {
+
+    // http://code.angularjs.org/1.2.1/docs/api/ngResource.$resource
+    var rest = $resource(
+      'rest/bookmarks/search/:name'
+    );
+
+    return rest;
+
+  }]);
+
+
+});
