987d2848263b29361bbc66866bf6bd46d6395354
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-07-18 10:48:49 -0300

    add TODO comment to src/app/main/routes.js

--- src/app/main/routes.js
+++ src/app/main/routes.js
@@ -1,3 +1,25 @@
+/*
+
+TODO:
+
+- add config to keep http credentials
+
+  [Gist] AngularJS Lesson Learned #2: Enabling HTTP Credentials | Mário Junior
+  https://gist.github.com/mariojunior/6175849
+
+    $httpProvider.defaults.withCredentials = true;
+
+  - check invalid session
+
+    [Gist] AngularJS Lesson Learned #1: Getting Invalid Session State | Mário Junior
+    https://gist.github.com/mariojunior/6175736
+
+- add config to allow access cross domain resource
+
+    // allow to access cross domain resource
+    $httpProvider.defaults.useXDomain = true;
+
+*/
 define(
 // require.js dependency injection
 [
