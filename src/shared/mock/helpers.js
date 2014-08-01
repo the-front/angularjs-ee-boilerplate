41f562dbf4d62dcf0cc8217f779017028dc459c6
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-01 09:41:15 -0300

    shared/mock/helpers reviewed

--- src/shared/mock/helpers.js
+++ src/shared/mock/helpers.js
@@ -32,17 +32,17 @@ function(module) {
 
       //---
 
-      ClassDef.prototype.isObject = function( obj ) {
-        return ( obj === Object( obj ) && ( 'object' === typeof obj ) );
+      ClassDef.prototype.isObject = function( value ) {
+        return ( value === Object( value ) && ( 'object' === typeof value ) );
       };
 
-      ClassDef.prototype.isFunction = function( object ) {
-        return !!(object && object.constructor && object.call && object.apply);
+      ClassDef.prototype.isFunction = function( value ) {
+        return !!( value && value.constructor && value.call && value.apply );
       };
 
       //---
 
-      ClassDef.prototype.extendsFn = function(fn, Extend) {
+      ClassDef.prototype.extendsFn = function( fn, Extend ) {
         if( this.isFunction( fn ) ) {
           var instance = null;
           if( this.isObject( Extend ) ) {
@@ -58,60 +58,60 @@ function(module) {
         return fn;
       };
 
-      ClassDef.prototype.extendsObj = function(destination, source) {
-        for (var property in source) destination[property] = source[property];
+      ClassDef.prototype.extendsObj = function( destination, source ) {
+        for( var property in source ) destination[property] = source[property];
         return destination;
       };
 
       //---
 
-      ClassDef.prototype.getIdFromURL = function(url, regexp) {
-        var arr = url.split(regexp); // ex.: /bookmarks\//
-        if(arr.length > 1) {
+      ClassDef.prototype.getIdFromURL = function( url, regexp ) {
+        var arr = url.split( regexp ); // ex.: /bookmarks\//
+        if( arr.length > 1 ) {
           var value = arr[1];
-          if( this.isNumber(value) ) value = parseInt(value, 10);
+          if( this.isNumber( value ) ) value = parseInt( value, 10 );
           return value;
         }
         return null;
       };
 
-      ClassDef.prototype.getValueFromURL = function(url, regexp) {
+      ClassDef.prototype.getValueFromURL = function( url, regexp ) {
         var find = null,
-            arr = url.split(regexp); // ex.: /bookmarks\/search\//
-        if(arr.length > 1) {
+            arr = url.split( regexp ); // ex.: /bookmarks\/search\//
+        if( arr.length > 1 ) {
           find = arr[1];
-          arr = find.split(/\?/);
-          if(arr.length > 1) find = arr[0];
+          arr = find.split( /\?/ );
+          if( arr.length > 1 ) find = arr[0];
         }
         return find;
       };
 
-      ClassDef.prototype.createResultMessage = function(_code, _message) {
+      ClassDef.prototype.createResultMessage = function( _code, _message ) {
         return {
           code: _code,
           message: _message
         };
       };
 
-      ClassDef.prototype.notFound = function(id) {
+      ClassDef.prototype.notFound = function( id ) {
         var msg = 'Not found';
-        if(id) msg += ' - id: ' + id;
-        return createResultMessage(404, msg);
+        if( id ) msg += ' - id: ' + id;
+        return createResultMessage( 404, msg );
       };
 
-      ClassDef.prototype.paginate = function(data, options) {
+      ClassDef.prototype.paginate = function( data, options ) {
 
-        if(!options || 'object' !== typeof options) {
+        if( !options || 'object' !== typeof options ) {
           options = {
             page: 1,
             size: 10
           };
         } else {
-          if(!options.hasOwnProperty('size')) options.size = 10;
-          else if(options.size < 2) options.size = 2;
+          if( !options.hasOwnProperty( 'size' ) ) options.size = 10;
+          else if( options.size < 2 ) options.size = 2;
         }
 
-        if(options.page <= 0) options.page = 1;
+        if( options.page <= 0 ) options.page = 1;
 
         //---
 
@@ -129,22 +129,22 @@ function(module) {
 
         count = 0;
         length = data.length;
-        i = ((options.page-1) * options.size);
+        i = ( ( options.page-1 ) * options.size );
         limit = options.size;
 
-        if(i < length) {
+        if( i < length ) {
           flag = true;
         } else {
           flag = false;
         }
 
-        while(flag) {
+        while( flag ) {
 
-          page.push(data[i]);
+          page.push( data[i] );
 
           i++; count++;
 
-          if(i<length && count<limit) {
+          if( i < length && count < limit ) {
             flag = true;
           } else {
             flag = false;
@@ -154,7 +154,7 @@ function(module) {
         result.count = length;
         result.data = page;
         result.page = options.page;
-        result.pages = Math.ceil(length / options.size);
+        result.pages = Math.ceil( length / options.size );
 
         return result;
       };
