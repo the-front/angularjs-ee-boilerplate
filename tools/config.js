5a08b68d1100e995f9486976299f1298246f8610
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-20 14:49:12 -0300

    tools/config.js updated #6
    added karma config

--- tools/config.js
+++ tools/config.js
@@ -1,27 +1,110 @@
 module.exports = function() {
+  'use strict';
 
-  return {
+  //----------------------------------------------------------------------------
 
-    paths: {
-      src: '../src',
-      build: '.temp',
-      dist: '../dist',
-      templates: 'templates',
-      editorconfig: '../.editorconfig'
-    },
+  var karma = {
 
-    frontend: {
-      webserver: 1337,
-      livereload: 9337 // default: 35729
-    },
+    reportsDir: 'tests_out',
 
-    // config proxy to application backend
-    backend: {
-      host: 'localhost',
-      port: 9000,
-      context: 'rest'
+    options: {
+
+      basePath: '../',
+
+      // frameworks to use
+      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+      frameworks: ['jasmine', 'requirejs'],
+
+      // https://karma-runner.github.io/0.12/config/files.html
+
+      // list of files / patterns to load in the browser
+      files: [
+        {pattern: 'src/vendor/angular.js/**/angular.{min.js, map, css}', included: true, served: true, watched:false},
+        {pattern: 'src/vendor/**/*.{js, map, css}', included: false, served: true, watched:false},
+
+        // load app source and test's specs
+        //'tools/tests/require.config.js',
+
+        // app source and tests specs
+        {pattern: 'src/**/*.{js,css}', included: false, served: true},
+
+        // cache templates
+        'src/**/*.html'
+      ],
+
+      // list of files to exclude
+      exclude: [
+        'src/require.config.js'
+      ],
+
+      // preprocess matching files before serving them to the browser
+      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: {
+        'src/**/*.html': ['ng-html2js'],
+        // source files, that you wanna generate coverage for
+        // do not include tests or libraries
+        // (these files will be instrumented by Istanbul)
+        '{src,src/!(vendor)/!(tests){,/!(tests)}}/!(require.load).js': ['coverage']
+      },
+
+      ngHtml2JsPreprocessor: {
+        // strip this from the file path
+        stripPrefix: 'src/'
+      },
+
+      // start these browsers
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+      browsers: ['PhantomJS', 'Chrome', 'Firefox'],
+
+      logLevel: 'INFO',
+
+      // web server port
+      port: 9876
+
     }
 
+  }; // @end: karma
+
+  //----------------------------------------------------------------------------
+
+  var paths = {
+
+    editorconfig: '../.editorconfig',
+    src: '../src',
+    build: '.temp',
+    dist: '../dist',                    // production files
+    reports: '../' + karma.reportsDir,  // karma reports output
+    templates: 'templates'
+
+  }; // @end: paths
+
+  //----------------------------------------------------------------------------
+
+  var frontend = {
+
+    webserver: 1337,
+    livereload: 9337 // default: 35729
+
+  }; // @end: frontend
+
+  //----------------------------------------------------------------------------
+
+  // config proxy to application backend
+  var backend = {
+
+    host: 'localhost',
+    port: 9000,
+    context: 'rest'
+
+  }; // @end: backend
+
+  //----------------------------------------------------------------------------
+
+  return {
+    karma:      karma,
+    paths:      paths,
+    frontend:   frontend,
+    backend:    backend
   };
 
 };
