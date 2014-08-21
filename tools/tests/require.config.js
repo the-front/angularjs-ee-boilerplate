250e758afffe518800cbabfb809b54dc1e4e7a3e
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-20 22:33:46 -0300

    require.config to karma tests defined #9

---
+++ tools/tests/require.config.js
@@ -0,0 +1,176 @@
+(function() {
+  'use strict';
+
+  //---
+
+  var allTemplateCacheFiles = [];
+  var TEMPLATE_CACHE_REGEXP = /html\.js$/i;
+
+  var pathToTemplateCache = function(path) {
+    return path.replace(/^\/base\/src\//, '').replace(/\.js$/, '');
+  };
+
+  Object.keys(window.__karma__.files).forEach(function(file) {
+    if (TEMPLATE_CACHE_REGEXP.test(file)) {
+      // Normalize paths to RequireJS module names.
+      allTemplateCacheFiles.push(pathToTemplateCache(file));
+    }
+  });
+
+  //---
+
+  require.config({
+
+    baseUrl: '/base/src',
+
+
+
+    // libraries dependencies (fallback support)
+    paths: {
+
+      jquery: [
+        'vendor/jquery/1.11.1/jquery.min'
+      ],
+
+      bootstrap: [
+        'vendor/bootstrap/3.2.0/js/bootstrap.min'
+      ],
+
+
+      angular: [
+        'vendor/angular.js/1.2.22/angular.min'
+      ],
+
+      angularRoute: [
+        'vendor/angular.js/1.2.22/angular-route.min'
+      ],
+
+      angularResource: [
+        'vendor/angular.js/1.2.22/angular-resource.min'
+      ],
+
+      angularAnimate: [
+        'vendor/angular.js/1.2.22/angular-animate.min'
+      ],
+
+
+      angularUiBootstrap: [
+        'vendor/angular-ui/bootstrap/0.11.0/ui-bootstrap-tpls.min'
+      ],
+
+
+      toaster: [
+        'vendor/toaster/0.3.0/toaster'
+      ],
+
+      ngProgress: [
+        'vendor/ngProgress/1.0.3/ngProgress.min'
+      ],
+
+
+      // @begin: mock libs
+      angularMocks: [
+        'vendor/angular.js/1.2.22/angular-mocks'
+      ],
+
+      angularMocksBackend: [
+        'vendor/angular-mocks-backend/0.1.4/angular-mocks-backend'
+      ],
+      // @end: mock libs
+
+
+      lokijs: [
+        'vendor/lokijs/0.0.1/lokijs.min'
+      ]
+
+    },
+
+    // define js scripts dependencies
+    shim: {
+
+      'bootstrap': {
+        deps: ['jquery']
+      },
+
+      'angular': {
+        deps: ['bootstrap'],
+        exports: 'angular'
+      },
+
+      'angularUiBootstrap': {
+        deps: ['bootstrap',  'angular']
+      },
+
+      'angularRoute': {
+        deps: ['angular']
+      },
+
+      'angularResource': {
+        deps: ['angular']
+      },
+
+      'angularAnimate': {
+        deps: ['angular']
+      },
+
+      'ngProgress': {
+        deps: ['angular']
+      },
+
+      'toaster': {
+        deps:['angularAnimate']
+      },
+
+
+      // @begin: mock js shim/deps
+      'angularMocks': {
+        deps: ['angular']
+      },
+
+      'angularMocksBackend': {
+        deps: ['angularMocks']
+      }
+      // @end: mock js shim/deps
+
+    },
+
+
+
+    priority: [
+      'angular'
+    ],
+
+    deps: [
+      'app/main/require.load',
+
+      'angularMocks',
+
+      './require.unit.load'
+    ],
+
+    callback: onRequireReadyHandler
+
+  });
+
+  function onRequireReadyHandler() {
+    console.log( 'onRequireReadyHandler' );
+
+    require(allTemplateCacheFiles, function() {
+      console.log( 'ng-html2js files loaded' );
+
+      angular.element(document).ready(function() {
+
+        console.log('bootstrap angular application');
+
+        // start angular app
+        angular.bootstrap(document, ['main']);
+
+        window.__karma__.start();
+
+      });
+
+    });
+
+  }
+
+})();
