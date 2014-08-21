4b451420fc871a84b33f28c4ec2d5da45ec39b11
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-20 22:41:51 -0300

    require tests unit loaded updated #9

--- src/require.unit.load.js
+++ src/require.unit.load.js
@@ -2,8 +2,9 @@ define(
 // require.js dependency injection
 [
   './app/main/tests/unit/require.load',
-  //'./app/home/tests/unit/require.load',
-  //'./app/about/tests/unit/require.load',
+  './app/home/tests/unit/require.load',
+  './app/about/tests/unit/require.load',
+  //'./app/help/tests/unit/require.load',
   //'./app/bookmarks/tests/unit/require.load',
 ],
 
