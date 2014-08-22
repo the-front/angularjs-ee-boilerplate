ee6eaf9d43526548a595444aa8acd4ae74cc5d0a
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-21 22:33:32 -0300

    /tools grunt browserSync updated #7
    added comment about error EMFILE on Mac OS

--- tools/helpers/grunt/config/browserSync.js
+++ tools/helpers/grunt/config/browserSync.js
@@ -1,4 +1,22 @@
-// https://github.com/shakyShane/browser-sync/wiki/options
+/*
+  https://github.com/shakyShane/browser-sync/wiki/options
+
+--------------------------------------------------------------------------------
+
+  How do I fix the error EMFILE: Too many opened files?
+
+    This is because of your system's max opened file limit.
+    For OSX the default is very low (256).
+    Temporarily increase your limit with `ulimit -n 10480`,
+    the number being the new max limit.
+
+    In some versions of OSX the above solution doesn't work.
+    In that case try `launchctl limit maxfiles 10480 10480` and restart your terminal.
+
+  https://github.com/gruntjs/grunt-contrib-watch#how-do-i-fix-the-error-emfile-too-many-opened-files
+
+  https://superuser.com/questions/261023/how-to-change-default-ulimit-values-in-mac-os-x-10-6
+*/
 
 module.exports = function(grunt) {
 
