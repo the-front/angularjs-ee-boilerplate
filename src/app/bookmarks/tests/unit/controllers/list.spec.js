f1a97d743a70b473e7b13fef606f87ca8002e00f
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-25 13:28:08 -0300

    bookmarks list controller event handlers test #9

--- src/app/bookmarks/tests/unit/controllers/list.spec.js
+++ src/app/bookmarks/tests/unit/controllers/list.spec.js
@@ -1,6 +1,6 @@
 describe('Testing Bookmarks List Controller', function() {
 
-  var ctrl, scope;
+  var location, ctrl, scope, rootScope, httpBackend;
 
   // excuted before each "it" is run
   beforeEach(function() {
@@ -9,12 +9,16 @@ describe('Testing Bookmarks List Controller', function() {
     module('bookmarks');
 
     // inject dependencies
-    inject(function($controller, $rootScope) {
+    inject(function($location, $controller, $rootScope, $httpBackend) {
       scope = $rootScope.$new();
 
       ctrl = $controller('BookmarksListCtrl', {
         $scope: scope
       });
+
+      location = $location;
+      rootScope = $rootScope;
+      httpBackend = $httpBackend;
     });
 
   });
@@ -22,6 +26,75 @@ describe('Testing Bookmarks List Controller', function() {
   it('should be defined', function() {
     expect(ctrl).toBeDefined(true);
   });
+
+
+  it("should change location to '/new/path'", function() {
+    // arrange
+    spyOn(location, 'path');
+
+    // act
+    location.path('/new/path');
+
+    //  assertions
+    expect(location.path).toHaveBeenCalledWith('/new/path');
+  });
+
+
+  describe("event handlers", function() {
+
+    beforeEach(function() {
+
+      // arrange
+      spyOn(location, 'path');
+
+      httpBackend.when('GET', 'rest/bookmarks?page=1&size=10')
+        .respond(function(method, url, data) {
+          data = {
+            data: [],
+            count: 0,
+            page: 1,
+            pages: 1
+          };
+          return [200, angular.copy(data)];
+        });
+
+    });
+
+    it("should handle 'bookmarks:add:event'", function() {
+
+      // act
+      rootScope.$emit('bookmarks:add:event', 'added');
+
+      //  assertions
+      expect(location.path).toHaveBeenCalledWith('/bookmarks');
+
+    });
+
+
+    it("should handle 'bookmarks:update:event'", function() {
+
+      // act
+      rootScope.$emit('bookmarks:update:event', 'updated');
+
+      //  assertions
+      expect(location.path).toHaveBeenCalledWith('/bookmarks');
+
+    });
+
+
+    it("should handle 'bookmarks:remove:event'", function() {
+
+      // act
+      rootScope.$emit('bookmarks:remove:event', 'removed');
+
+      //  assertions
+      expect(location.path).toHaveBeenCalledWith('/bookmarks');
+
+    });
+
+  });
+
+
 
   // TODO: define test's
 
