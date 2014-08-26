abc363873e722183332f026712f71d540ac17025
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-26 16:04:12 -0300

    src/require.unit.load.js code reviewed #8

--- src/require.unit.load.js
+++ src/require.unit.load.js
@@ -3,11 +3,11 @@ define(
 [
   // TODO: define and load /shared tests unit
 
-  './app/main/tests/unit/require.load',
-  './app/home/tests/unit/require.load',
-  './app/about/tests/unit/require.load',
-  './app/help/tests/unit/require.load',
-  './app/bookmarks/tests/unit/require.load'
+  'app/main/tests/unit/require.load',
+  'app/home/tests/unit/require.load',
+  'app/about/tests/unit/require.load',
+  'app/help/tests/unit/require.load',
+  'app/bookmarks/tests/unit/require.load'
 ],
 
 // require.js module scope
