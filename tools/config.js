d3ac4609ec8ddbdfc7b7565d38fbf65f5084090c
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-27 15:35:17 -0300

    karma coverage preprocessor reviewed #10
    files match

--- tools/config.js
+++ tools/config.js
@@ -42,7 +42,7 @@ module.exports = function() {
         // source files, that you wanna generate coverage for
         // do not include tests or libraries
         // (these files will be instrumented by Istanbul)
-        '{src,src/!(vendor)/!(tests){,/!(tests)}}/!(require.load).js': ['coverage']
+        '{src,src/!(vendor)/!(tests){,/!(tests){,/!(tests){,/!(tests)}}}}/!(require.load).js': ['coverage']
       },
 
       ngHtml2JsPreprocessor: {
