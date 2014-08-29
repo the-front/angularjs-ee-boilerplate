f08c1a67d2645db5730c3c5937054588951c245b
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-08-29 15:33:48 -0300

    tools grunt open reports defined #7

--- tools/helpers/grunt/config/open.js
+++ tools/helpers/grunt/config/open.js
@@ -8,6 +8,14 @@ grunt.config('open', {
 
   reports: {
     path: '<%= project.paths.reports %>'
+  },
+
+  karma_report_coverage: {
+    path: 'http://localhost:<%= project.reports.port.webserver %>/coverage'
+  },
+
+  karma_report_jasmine: {
+    path: 'http://localhost:<%= project.reports.port.webserver %>/jasmine'
   }
 
 });
