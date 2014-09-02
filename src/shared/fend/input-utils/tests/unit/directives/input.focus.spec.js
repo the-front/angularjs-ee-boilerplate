be0d667c271d9c3d4726932871ae15e3822aa21c
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-09-02 10:54:47 -0300

    src/shared/fend-utils input.focus test defined #10

---
+++ src/shared/fend/input-utils/tests/unit/directives/input.focus.spec.js
@@ -0,0 +1,57 @@
+describe("Testing fend.input.utils Input Focus Directive", function() {
+
+  var scope, compile, doc, body;
+
+  beforeEach(function() {
+
+    // load the module
+    module('fend.input.utils');
+
+    // inject dependencies
+    inject(function($rootScope, $compile, $document) {
+      scope = $rootScope.$new();
+      compile = $compile;
+      doc = $document;
+    });
+
+    body = angular.element(doc[0].body);
+
+  });
+
+  describe("as attribute", function() {
+
+    var fieldName = 'inputField',
+        modelName = 'inputFieldModel',
+        focusName = 'focusInputField';
+
+    var element;
+
+    beforeEach(function() {
+      scope[focusName] = false;
+      scope[modelName] = 'some value';
+
+      element = compile('<input type="text" name="'+fieldName+'" ng-model="'+modelName+'" fend-focus="'+focusName+'">')(scope);
+      scope.$digest();
+      body.append(element); // important to receive focus test
+    });
+
+
+    it("should have fend-focus attribute equals to " + focusName, function() {
+      // assertions
+      expect(element.attr('fend-focus')).toEqual(focusName);
+    });
+
+
+    it("should receive focus", function() {
+      // act
+      scope[focusName] = true;
+      scope.$digest();
+
+      // assertions
+      expect(document.activeElement === element[0]).toBeTruthy();
+    });
+
+
+  });
+
+});
--- src/shared/fend/input-utils/tests/unit/require.load.js
+++ src/shared/fend/input-utils/tests/unit/require.load.js
@@ -2,6 +2,7 @@ define(
 // require.js dependency injection
 [
   './module.spec',
+  './directives/input.focus.spec',
   './factories/input.focus.spec'
 ],
 
