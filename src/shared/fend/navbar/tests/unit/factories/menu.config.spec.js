a215bc62e2928a3abbccfbab1ea64fe2dfeb544c
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-09-04 20:36:26 -0300

    navbar tests code review
    src/shared/fend/navbar

--- src/shared/fend/navbar/tests/unit/factories/menu.config.spec.js
+++ src/shared/fend/navbar/tests/unit/factories/menu.config.spec.js
@@ -23,10 +23,14 @@ describe("Testing fend.navbar MenuConfig Factory", function() {
 
 
   it("should be registered", function() {
-    expect(menu).not.toEqual(null);
+
+    // assertions
+    expect(menu).toBeDefined();
+
   });
 
   it("should config menu", function() {
+
     // act
     menu.addMenuItem('Home', '');
     menu.addMenuItem('UC1', 'uc1');
@@ -37,36 +41,44 @@ describe("Testing fend.navbar MenuConfig Factory", function() {
     expect(rootScope.menuItems).toBeDefined();
     expect(rootScope.menuItems.left.length).toEqual(3);
     expect(rootScope.menuItems.right.length).toEqual(1);
+
   });
 
   describe("change location", function() {
 
     beforeEach(function() {
+
       // arrange
       menu.addMenuItem('Home', '');
       menu.addMenuItem('UC1', 'uc1');
       menu.addMenuItem('UC2', 'uc2');
+
     });
 
     it("should select UC1 menu item", function() {
+
       // act
       location.path('/uc1');
       rootScope.$digest();
 
       // assertions
       expect(rootScope.menuItems.left[1].css).toEqual('active');
+
     });
 
     it("should select UC2 menu item", function() {
+
       // act
       location.path('/uc2/new');
       rootScope.$digest();
 
       // assertions
       expect(rootScope.menuItems.left[2].css).toEqual('active');
+
     });
 
     it("should UC2 menu item stay selected", function() {
+
       // act
       location.path('/uc2');
       rootScope.$digest();
@@ -75,9 +87,11 @@ describe("Testing fend.navbar MenuConfig Factory", function() {
 
       // assertions
       expect(rootScope.menuItems.left[2].css).toEqual('active');
+
     });
 
     it("should change menu item selected", function() {
+
       // act
       location.path('/uc1');
       rootScope.$digest();
@@ -87,9 +101,11 @@ describe("Testing fend.navbar MenuConfig Factory", function() {
       // assertions
       expect(rootScope.menuItems.left[1].css).toEqual('');
       expect(rootScope.menuItems.left[2].css).toEqual('active');
+
     });
 
     it("should unselect UC2 menu item", function() {
+
       // act
       location.path('/uc2');
       rootScope.$digest();
@@ -98,9 +114,11 @@ describe("Testing fend.navbar MenuConfig Factory", function() {
 
       // assertions
       expect(rootScope.menuItems.left[2].css).toEqual('');
+
     });
 
     it("should not select none menu item", function() {
+
       // act
       location.path('/undefined');
       rootScope.$digest();
@@ -109,6 +127,7 @@ describe("Testing fend.navbar MenuConfig Factory", function() {
       expect(rootScope.menuItems.left[0].css).toEqual('');
       expect(rootScope.menuItems.left[1].css).toEqual('');
       expect(rootScope.menuItems.left[2].css).toEqual('');
+
     });
 
   });
