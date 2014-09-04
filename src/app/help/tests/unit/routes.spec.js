e6acacbdf21deeba3cf30cc5810ddb48aad68b41
Erko Bridee de Almeida Cabrera <erko.bridee@gmail.com>
2014-09-04 16:21:25 -0300

    src/app/help tests code review

--- src/app/help/tests/unit/routes.spec.js
+++ src/app/help/tests/unit/routes.spec.js
@@ -2,17 +2,24 @@ describe('Angular.js \'help\' Routes', function() {
 
   var route;
 
+  // excuted before each "it" is run
   beforeEach(function() {
+
+    // load the module
     module('help');
 
+    // inject dependencies
     inject(function($route) {
       route = $route;
     });
+
   });
 
   /* only to check if injection work fine */
   it('should be defined', function() {
-    expect(route).not.toEqual(null);
+
+    expect(route).toBeDefined();
+
   });
 
   describe('Routes Map', function() {
