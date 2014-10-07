5a50c94bcee663acc199bebd1cae3da5a79e07bb
Erko Bridee <erko.bridee@gmail.com>
2014-10-06 23:52:03 -0300

    src/require.unit.load.js #18 #19

--- src/require.unit.load.js
+++ src/require.unit.load.js
@@ -1,17 +1,15 @@
-define(
-// require.js dependency injection
-[
-  'shared/fend/progressbar-loading/tests/unit/require.load',
-  'shared/fend/input-utils/tests/unit/require.load',
-  'shared/fend/navbar/tests/unit/require.load',
-  'shared/fend/pagination/tests/unit/require.load',
+define(function(require) {
+  'use strict';
 
-  'app/main/tests/unit/require.load',
-  'app/home/tests/unit/require.load',
-  'app/about/tests/unit/require.load',
-  'app/help/tests/unit/require.load',
-  'app/bookmarks/tests/unit/require.load'
-],
+  require('shared/fend/progressbar-loading/tests/unit/require.load');
+  require('shared/fend/input-utils/tests/unit/require.load');
+  require('shared/fend/navbar/tests/unit/require.load');
+  require('shared/fend/pagination/tests/unit/require.load');
 
-// require.js module scope
-function() {});
+  require('app/main/tests/unit/require.load');
+  require('app/home/tests/unit/require.load');
+  require('app/about/tests/unit/require.load');
+  require('app/help/tests/unit/require.load');
+  require('app/bookmarks/tests/unit/require.load');
+
+});
