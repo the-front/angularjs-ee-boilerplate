d7749e8c21bd65b6649908ad975bbe34e583d9d0
Erko Bridee <erko.bridee@gmail.com>
2014-11-20 16:41:38 -0200

    require.config.js for karma runner updated

--- tools/helpers/tests/require.config.js
+++ tools/helpers/tests/require.config.js
@@ -52,7 +52,7 @@
       ],
 
       uiRouter: [
-        'vendor/libs/angular-ui/ui-router/0.2.11/angular-ui-router.min'
+        'vendor/angular-ui/ui-router/0.2.11/angular-ui-router.min'
       ],
 
       toaster: [
