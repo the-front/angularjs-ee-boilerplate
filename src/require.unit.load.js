664a7356a937d90388dd22fbbd6a51f8182b6b2e
Erko Bridee <erko.bridee@gmail.com>
2014-11-21 16:54:48 -0200

    unit tests loader updated #25
    keep only main tests enabled

--- src/require.unit.load.js
+++ src/require.unit.load.js
@@ -1,16 +1,16 @@
 define(function(require) {
   'use strict';
 
-  require('shared/fend/progressbar-loading/tests/unit/package');
-  require('shared/fend/input-utils/tests/unit/package');
-  require('shared/fend/navbar/tests/unit/package');
-  require('shared/fend/pagination/tests/unit/package');
+  // require('shared/fend/progressbar-loading/tests/unit/package');
+  // require('shared/fend/input-utils/tests/unit/package');
+  // require('shared/fend/navbar/tests/unit/package');
+  // require('shared/fend/pagination/tests/unit/package');
 
   require('app/main/tests/unit/package');
-  require('app/home/tests/unit/package');
-  require('app/about/tests/unit/package');
-  require('app/help/tests/unit/package');
-  require('app/bookmarks/tests/unit/package');
+  // require('app/home/tests/unit/package');
+  // require('app/about/tests/unit/package');
+  // require('app/help/tests/unit/package');
+  // require('app/bookmarks/tests/unit/package');
 
 
   // TODO: add here tests unit module to load
