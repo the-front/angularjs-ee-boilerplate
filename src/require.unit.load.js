84e79125d3b9f2e2e2485fce781203a67ded9e71
Erko Bridee <erko.bridee@gmail.com>
2014-11-21 19:09:40 -0200

    load all specs #25

--- src/require.unit.load.js
+++ src/require.unit.load.js
@@ -1,15 +1,15 @@
 define(function(require) {
   'use strict';
 
-  // require('shared/fend/progressbar-loading/tests/unit/package');
-  // require('shared/fend/input-utils/tests/unit/package');
-  // require('shared/fend/navbar/tests/unit/package');
-  // require('shared/fend/pagination/tests/unit/package');
+  require('shared/fend/progressbar-loading/tests/unit/package');
+  require('shared/fend/input-utils/tests/unit/package');
+  require('shared/fend/navbar/tests/unit/package');
+  require('shared/fend/pagination/tests/unit/package');
 
-  // require('app/main/tests/unit/package');
-  // require('app/home/tests/unit/package');
-  // require('app/about/tests/unit/package');
-  // require('app/help/tests/unit/package');
+  require('app/main/tests/unit/package');
+  require('app/home/tests/unit/package');
+  require('app/about/tests/unit/package');
+  require('app/help/tests/unit/package');
   require('app/bookmarks/tests/unit/package');
 
 
