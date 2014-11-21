fe5e5e9915261bb51d75a8e0c0659f6b022526e2
Erko Bridee <erko.bridee@gmail.com>
2014-11-21 09:14:54 -0200

    bookmarks routes.js update to ui router #25

--- src/app/bookmarks/routes.js
+++ src/app/bookmarks/routes.js
@@ -7,44 +7,63 @@ define(function(require) {
 
   //---
 
-  configure.$inject = ['$routeProvider'];
+  configure.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-  function configure($routeProvider) {
+  function configure($stateProvider, $urlRouterProvider) {
 
-    $routeProvider
-      .when(
-        '/bookmarks',
-        {
-          templateUrl   : 'app/bookmarks/templates/list.html',
-          controller    : 'BookmarksListCtrl',
-          controllerAs  : 'vm'
+    $urlRouterProvider
+      .when('/bookmarks', '/bookmarks/list'); // default
+
+    $stateProvider
+      .state('bookmarks', {
+        abstract: true,
+        url: '/bookmarks',
+        views: {
+          'master': {
+            templateUrl: 'app/main/templates/layout.html'
+          }
         }
-      )
-      .when(
-        '/bookmarks/search',
-        {
-          templateUrl   : 'app/bookmarks/templates/search.html',
-          controller    : 'BookmarksSearchCtrl',
-          controllerAs  : 'vm',
+      })
+      .state('bookmarks.list', {
+        url: '/list',
+        views: {
+          'content@bookmarks': {
+            templateUrl   : 'app/bookmarks/templates/list.html',
+            controller    : 'BookmarksListCtrl',
+            controllerAs  : 'vm'
+          }
         }
-      )
-      .when(
-        '/bookmarks/new',
-        {
-          templateUrl   : 'app/bookmarks/templates/form.html',
-          controller    : 'BookmarksNewCtrl',
-          controllerAs  : 'vm'
+      })
+      .state('bookmarks.search', {
+        url: '/search',
+        views: {
+          'content@bookmarks': {
+            templateUrl   : 'app/bookmarks/templates/search.html',
+            controller    : 'BookmarksSearchCtrl',
+            controllerAs  : 'vm'
+          }
         }
-      )
-      .when(
-        '/bookmarks/edit/:id',
-        {
-          templateUrl   : 'app/bookmarks/templates/form.html',
-          controller    : 'BookmarksEditCtrl',
-          controllerAs  : 'vm'
+      })
+      .state('bookmarks.new', {
+        url: '/new',
+        views: {
+          'content@bookmarks': {
+            templateUrl   : 'app/bookmarks/templates/form.html',
+            controller    : 'BookmarksNewCtrl',
+            controllerAs  : 'vm'
+          }
         }
-      )
-    ;
+      })
+      .state('bookmarks.edit', {
+        url: '/edit/:id',
+        views: {
+          'content@bookmarks': {
+            templateUrl   : 'app/bookmarks/templates/form.html',
+            controller    : 'BookmarksEditCtrl',
+            controllerAs  : 'vm'
+          }
+        }
+      });
 
   }
 
