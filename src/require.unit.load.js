2f78b41f7be68a9ecf6570d5f72d5632049ac818
Erko Bridee <erko.bridee@gmail.com>
2014-11-21 17:15:35 -0200

    load only about specs #25

--- src/require.unit.load.js
+++ src/require.unit.load.js
@@ -6,9 +6,9 @@ define(function(require) {
   // require('shared/fend/navbar/tests/unit/package');
   // require('shared/fend/pagination/tests/unit/package');
 
-  require('app/main/tests/unit/package');
+  // require('app/main/tests/unit/package');
   // require('app/home/tests/unit/package');
-  // require('app/about/tests/unit/package');
+  require('app/about/tests/unit/package');
   // require('app/help/tests/unit/package');
   // require('app/bookmarks/tests/unit/package');
 
