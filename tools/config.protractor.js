d57deddc3e88af7f8f34913339ce154328ecc8c3
Erko Bridee <erko.bridee@gmail.com>
2014-12-02 20:41:11 -0200

    tools protractor config reviewed

--- tools/config.protractor.js
+++ tools/config.protractor.js
@@ -3,19 +3,16 @@ exports.config = {
 
   // will use local selenium server jar
   // seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://selenium.host:4444/wd/hub',
+
 
   // default web browser: Google Chrome
-  /*
-  capabilities: {
-    'browserName': 'firefox'
-  },
-  */
+  // capabilities: {
+  //   // 'browserName': 'safari'
+  //   // 'browserName': 'firefox'
+  //   // 'browserName': 'internet explorer'
+  // },
 
-  /*
-  capabilities: {
-    'browserName': 'internet explorer'
-  },
-  */
 
   /*
   multiCapabilities: [
@@ -40,6 +37,24 @@ exports.config = {
     }
   ],
   */
+
+  framework: 'jasmine',
+
+  // See the full list at https://github.com/juliemr/minijasminenode/tree/jasmine1
+  jasmineNodeOpts: {
+    // If true, display spec names.
+    isVerbose: true,
+    // If true, print colors to the terminal.
+    showColors: true,
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 30000,
+    // If true, print timestamps for failures
+    showTiming: true,
+    // Print failures in real time.
+    realtimeFailure: false
+  },
 
   specs: [
     '../src/**/tests/e2e/*.spec.js'
