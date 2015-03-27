5f25a4a4d9d5358d633edb33026869c7e62f21a0
Erko Bridee <erko.bridee@gmail.com>
2015-03-27 11:24:52 -0300

    #35 unused files removed

--- tools/gulp/tasks/styles.js
+++ tools/gulp/tasks/styles.js
@@ -12,4 +12,15 @@ module.exports = function(gulp, $) {
 
   });
 
+  gulp.task('sass', function() {
+
+    $.log("Building css files...");
+
+    //---
+
+    // TODO: define
+    $.log("define sass build");
+
+  });
+
 };
