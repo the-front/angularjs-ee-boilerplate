ec617f3c05d7887681dffa2e6f4b1b0346edc9e4
Erko Bridee <erko.bridee@gmail.com>
2015-03-27 11:25:26 -0300

    #35 tools sharedStreams reviewed

--- tools/gulp/helpers/sharedStreams.js
+++ tools/gulp/helpers/sharedStreams.js
@@ -1,13 +1,16 @@
 var gulp      = require('gulp');
 var $         = require('./$');
 
-var fs        = require('fs');
-var glob      = require('glob').sync;
-
 // shared streams to gulp tasks
 var streams = $.streams = {};
 
-// TODO: review
+//--
+
+var outputCssDir = (
+  $.is.release ?
+    $.config.paths.dist :
+    $.config.paths.build
+);
 
 //---
 
@@ -17,54 +20,32 @@ streams.autoprefix = function() {
 
 //---
 
-/**
- * Builds the entire component library javascript.
- * @param {boolean} isRelease Whether to build in release mode.
- */
-streams.buildJS = function( isRelease ) {
+streams.less = function() {
 
-  isRelease = isRelease || $.is.release;
-
-  $.log("building js files...");
-
-  var jsBuildStream = gulp
-    .src(
-      $.config
-        .js.project
-        .base.concat([
-          $.path.join( $.config.glob.paths, '*.js')
-        ])
-    )
-    .pipe( $.utils.filterNonCodeFiles() )
-    .pipe( $.utils.buildNgDefinition() )
-    .pipe( $.plumber() )
-    .pipe( $.ngAnnotate() );
-
-  var themeBuildStream = gulp
-    .src(
-      $.config
-        .styles.project
-        .base.theme.concat([
-          $.path.join( $.config.glob.paths, '*-theme.scss' )
-        ])
-    )
-    .pipe( $.concat('default-theme.scss') )
-    .pipe( $.utils.hoistScssVariables() )
+  return gulp
+    .src( $.config.styles.less.main )
     .pipe( $.if( $.is.debug, $.debug() ) )
-    .pipe( $.sass({ onError: $.onError }) )
-    .pipe( gulp.dest( $.config.paths.outputDir ) )
-    .pipe( $.utils.cssToNgConstant( $.pkg.ngPackage + '.core', '$MD_THEME_CSS') );
-
-  var jsReleaseStream = $.lazypipe()
-    .pipe( $.uglify, { preserveComments: 'some' } )
-    .pipe( $.rename, { suffix: '.min' } )
-    .pipe( gulp.dest, $.config.paths.outputDir );
-
-
-  return $.series( jsBuildStream, themeBuildStream )
-    .pipe( $.concat( $.pkg.name + '.js') )
+    .pipe( $.plumber() )
+    .pipe( $.less() )
+    .pipe( autoprefix() )
     .pipe( $.insert.prepend( $.config.banner ) )
-    .pipe( gulp.dest( $.config.paths.outputDir ) )
-    .pipe( $.if( isRelease, jsReleaseStream() ));
+    .pipe( $.if( $.is.release, $.minifyCss() ) )
+    .pipe( gulp.dest( outputCssDir ) )
+    .on( 'error', $.onError );
+
+};
+
+streams.sass = function() {
+
+  return gulp
+    .src( $.config.styles.sass.main )
+    .pipe( $.if( $.is.debug, $.debug() ) )
+    .pipe( $.plumber() )
+    .pipe( $.sass() )
+    .pipe( autoprefix() )
+    .pipe( $.insert.prepend( $.config.banner ) )
+    .pipe( $.if( $.is.release, $.minifyCss() ) )
+    .pipe( gulp.dest( outputCssDir ) )
+    .on( 'error', $.onError );
 
 };
