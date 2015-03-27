a59a3c44003b372b99afd5a7bdcc10c838447b11
Erko Bridee <erko.bridee@gmail.com>
2015-03-27 14:55:49 -0300

    #35 gulp clean tasks reviewed

--- tools/gulp/tasks/clean.js
+++ tools/gulp/tasks/clean.js
@@ -1,5 +1,11 @@
 module.exports = function(gulp, $) {
 
+  gulp.task('clean:build', function( done ) {
+    $.del([
+      $.config.paths.build
+    ], done);
+  });
+
   gulp.task('clean:dist', function( done ) {
     $.del([
       $.config.paths.dist
@@ -8,6 +14,6 @@ module.exports = function(gulp, $) {
 
   // TODO: review
 
-  gulp.task('clean', ['clean:dist']);
+  gulp.task('clean', ['clean:dist', 'clean:build']);
 
 };
