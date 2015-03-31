8a853c0d9dbf32242a5675a04b0d686c8e372d40
Erko Bridee <erko.bridee@gmail.com>
2015-03-31 09:22:57 -0300

    #35 maven setup script updated

--- tools/scripts/maven.js
+++ tools/scripts/maven.js
@@ -14,11 +14,6 @@ async_exec('npm install shelljs', function (err, stdout, stderr) {
 
   echo('----------------------------------------');
 
-  echo('Installing gulp...\n');
-  exec('npm install gulp');
-
-  echo('----------------------------------------');
-
   echo('Installing node dependencies...\n');
   exec('npm install');
 
